b7ac4452c908c1776116546a29c93b8ffbbac8d1
YEOKYEONGMIN <84887067+YEOKYEONGMIN@users.noreply.github.com>
2021-08-31 16:00:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ex11/MainActivity.java
@@ -0,0 +1,228 @@
+package com.example.ex11;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Gallery;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.nio.channels.GatheringByteChannel;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    TextView toastText;
+    Integer[] posterID ={
+            R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04,
+            R.drawable.mov05, R.drawable.mov06, R.drawable.mov07, R.drawable.mov08,
+            R.drawable.mov09, R.drawable.mov10, R.drawable.mov11, R.drawable.mov12,
+            R.drawable.mov13, R.drawable.mov14, R.drawable.mov15, R.drawable.mov16,
+            R.drawable.mov17, R.drawable.mov18, R.drawable.mov19, R.drawable.mov20,
+            R.drawable.mov21, R.drawable.mov22
+    };
+    String[] posetTitle ={
+            "써니", "완득이", "괴물", "라디오스타", "비열한거리", "왕의남자"," 아일랜드",
+            "웰컴투동막골", "헬보이", "Back to the Future", "여인의향기", "쥬라기공원",
+            "톰행크스 포레스트검프", "Gruoundhog Day", "혹성탈출 진화의시작", "아름다운 비행",
+            "내이름은 칸", "해리포터 죽음의성물2", "마더", "킹콩을 들다", "쿵푸팬더2",
+            "짱구는 못말려"
+    };
+    View toastView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.ex11_01);
+//        setContentView(R.layout.ex11_04);
+//        setContentView(R.layout.ex11_06);
+        setContentView(R.layout.ex11_12);
+
+
+
+//        ex1();
+//        ex4();
+//        grid();
+        gallery();
+
+
+    }
+
+    void ex1(){
+        setTitle("리스트뷰 테스트");
+
+        final  String[] mid = {"히어로즈", "24시", "로스트", " 로스트룸", "스몰빌", "탐정몽크",
+                "빅뱅이론", "프렌즈", "덱스터", "글리", "가쉽걸", "테이큰", "슈퍼내추럴", "브이"};
+
+        ListView list = (ListView) findViewById(R.id.listView1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_multiple_choice, mid);
+        list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), mid[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    void ex4(){
+        final ArrayList<String> midList = new ArrayList<String>();
+        ListView list = (ListView) findViewById(R.id.listView1);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, midList);
+        list.setAdapter(adapter);
+
+        final EditText edtItem = (EditText) findViewById(R.id.edtItem);
+        Button btnAdd = (Button) findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                midList.add(edtItem.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                midList.remove(i);
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+    }
+    void grid(){
+        setTitle("영화 포스터");
+
+        final GridView gv = (GridView) findViewById(R.id.gridView1);
+        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        gv.setAdapter(gAdapter);
+    }
+    public class MyGridAdapter extends BaseAdapter{
+        Context context;
+        public  MyGridAdapter(Context c){
+            context = c;
+        }
+
+        @Override
+        public int getCount() {
+            return posterID.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ImageView imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(200,300));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setPadding(5,5,5,5);
+
+            imageView.setImageResource(posterID[i]);
+
+            final int pos = i;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    View dialogView = (View) View.inflate(MainActivity.this,
+                            R.layout.dialog,null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.ivPoster);
+                    ivPoster.setImageResource(posterID[i]);
+                    dlg.setTitle(posetTitle[i]);
+                    dlg.setIcon(R.drawable.movie_icon);
+                    dlg.setView(dialogView);
+                    dlg.setNegativeButton("닫기", null);
+                    dlg.show();
+                }
+            });
+
+            return imageView;
+        }
+    }
+
+    void gallery(){
+        setTitle("갤러리 영화 포스터");
+        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
+        MyGalleryAdapter galAdapter = new MyGalleryAdapter(this);
+        gallery.setAdapter(galAdapter);
+    }
+
+    public class MyGalleryAdapter extends BaseAdapter{
+        Context context;
+        public MyGalleryAdapter(Context c){
+            context = c;
+        }
+
+        @Override
+        public int getCount() {
+            return posterID.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ImageView imageView = new ImageView(context);
+            imageView.setLayoutParams(new Gallery.LayoutParams(200,300));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setPadding(5,5,5,5);
+
+            imageView.setImageResource(posterID[i]);
+
+            final int pos = i;
+            imageView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    ImageView ivPoster = (ImageView) findViewById(R.id.ivPoster);
+                    ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    ivPoster.setImageResource(posterID[i]);
+                    Toast toast = new Toast(MainActivity.this);
+                    toastView = (View) View.inflate(MainActivity.this,R.layout.toast,null);
+                    toastText = (TextView) toastView.findViewById(R.id.toastText);
+                    toastText.setText(posetTitle[i]);
+                    toast.setView(toastView);
+                    toast.show();
+                    return false;
+                }
+            });
+
+            return imageView;
+        }
+    }
+}
